967728fbceed067219071eeb8731de0a2f3a1359
benzssuk <49448736+BenzSSuk@users.noreply.github.com>
2024-11-10 17:43:27 +0700

    feat: avoiding update duplicate transaction

--- src/payment/update_balance_from_payment.py
+++ src/payment/update_balance_from_payment.py
@@ -22,48 +22,67 @@ def checkSysPathAndAppend(path, stepBack = 0):
 
 folderFile, filename = os.path.split(os.path.realpath(__file__))
 FOLDER_PROJECT = checkSysPathAndAppend(folderFile, 2)
+FOLDER_DATA = pjoin(FOLDER_PROJECT, 'data')
+FOLDER_GGSHEET = pjoin(FOLDER_DATA, 'ggsheet')
 
-import lib.DataProcessing as wedolib
-
-# load current balance
-path_balance = pjoin(FOLDER_PROJECT, 'account', 'wedo_badminton_balance.csv')
-df_balance = pd.read_csv(path_balance)
-df_balance.set_index('player_code', inplace=True)
+import lib.DataProcessing as mylib
 
 PATH_USER_CODE = pjoin(FOLDER_PROJECT, 'data', 'user_id', 'user_code.csv')
 df_user_code = pd.read_csv(PATH_USER_CODE)
 df_user_code.set_index('name_on_form', inplace=True)
 
 # update with payment from google form
-FOLDER_PAY = pjoin(FOLDER_PROJECT, 'record', 'payment', 'ggform')
-list_dir, list_folder, list_name = wedolib.findFile(FOLDER_PAY, '*.csv', 0)
+FOLDER_PAY = pjoin(FOLDER_GGSHEET, 'landing', 'payment')
+list_dir, list_folder, list_name = mylib.findFile(FOLDER_PAY, '*.csv', 0)
 n_files = len(list_name)
 if n_files > 0:
     for ifile in range(n_files):
         filename = list_name[ifile]
-        path_file = list_dir[ifile]
-        df_pay = pd.read_csv(path_file)
+        path_file_player_pay = list_dir[ifile]
+
+        # check is already update this file
+        folder_checked = pjoin(FOLDER_GGSHEET, 'checked', 'payment')
+        path_file_player_pay_checked = pjoin(folder_checked, filename)
+        if os.path.exists(path_file_player_pay_checked):
+            # already checked this file
+            continue
+
+        df_pay = pd.read_csv(path_file_player_pay)
+        df_pay.columns = ['timestamp', 'path_slip', 'payment', 'name']
 
         n_user = df_pay.shape[0]
-        print(f'updating balance from payment {filename}')
+        
         for i in range(n_user):
             name_from_form = df_pay['name'][i]
+            player_pay = df_pay['payment'][i]
+            timestamp_pay = df_pay['timestamp'][i]
+
             player_code = df_user_code.loc[name_from_form, 'player_code']
             player_team = df_user_code.loc[name_from_form, 'team']
             player_name = df_user_code.loc[name_from_form, 'name']
 
-            player_pay = df_pay['payment'][i]
-            timestamp_pay = df_pay['timestamp'][i]
-            balance_prev = df_balance.loc[player_code, 'balance']
-            balance_current = balance_prev + player_pay 
-            df_balance.loc[player_code, 'balance'] = balance_current
+            print(f'updating payment {timestamp_pay} {player_code} {player_pay} bath')
 
+            # load account
+            filename_account_player = f'balance_{player_team}_{player_name}.csv'
+            folder_account_player = pjoin(FOLDER_PROJECT, 'account', 'by_player', player_team)
+            path_account_player = pjoin(folder_account_player, filename_account_player)
+            dfBalancePlayerHist = pd.read_csv(path_account_player, index_col=False)
+            
+            # check is already update this transaction or not
+            if timestamp_pay in dfBalancePlayerHist['date'].to_list():
+                print(f'found this transaction {timestamp_pay} in player histrory, skipping...')
+                continue
+            
+            # calculate balance
+            balance_prev = dfBalancePlayerHist['balance'].iloc[-1]
+            balance_current = balance_prev + player_pay
+            
             # update player history
             filename_player_balance = f'balance_{player_team}_{player_name}.csv'
             FOLDER_ACCOUNT_PLAYER = os.path.join(FOLDER_PROJECT, 'account', 'by_player', player_team)
-            if not os.path.exists(FOLDER_ACCOUNT_PLAYER):
-                os.makedirs(FOLDER_ACCOUNT_PLAYER)
             PATH_ACCOUNT_PLAYER = os.path.join(FOLDER_ACCOUNT_PLAYER, filename_player_balance)
+
             # PATH_ACCOUNT_PLAYER_ONEDRIVE = os.path.join(FOLDER_BADMINTON_ONEDRIVE, 'account', 'by_player', filename_player_balance)
             dictHist = {
                 "date": timestamp_pay,
@@ -80,29 +99,19 @@ if n_files > 0:
             dfBalancePlayerHistNew = pd.DataFrame(dictHist, index=[0])
             if os.path.exists(PATH_ACCOUNT_PLAYER):
                 # load and append
-                dfBalancePlayerHist = pd.read_csv(PATH_ACCOUNT_PLAYER, index_col=False)
-                # dfBalancePlayerHist = dfBalancePlayerHist.append(dfBalancePlayerHistNew)
-                dfBalancePlayerHist = pd.concat([dfBalancePlayerHist, dfBalancePlayerHistNew], ignore_index=True)
-                dfBalancePlayerHist.to_csv(PATH_ACCOUNT_PLAYER, index=False)
+                dfBalancePlayerHistUpdate = pd.concat([dfBalancePlayerHist, dfBalancePlayerHistNew], ignore_index=True)
+                dfBalancePlayerHistUpdate.to_csv(PATH_ACCOUNT_PLAYER, index=False)
 
             else:
+                if not os.path.exists(FOLDER_ACCOUNT_PLAYER):
+                    os.makedirs(FOLDER_ACCOUNT_PLAYER)
                 # create new 
+                print(f'create new account player {filename_player_balance}...')
                 dfBalancePlayerHistNew.to_csv(PATH_ACCOUNT_PLAYER, index=False)
 
-    df_balance.reset_index(inplace=True)
-    df_balance.to_csv(path_balance, index=False)
-
-    for ifile in range(n_files):
-        path_ori = list_dir[ifile]
-        filename = list_name[ifile]
-
-        path_new = pjoin(FOLDER_PROJECT, 'data', 'checked', 'payment', filename)
-        if os.path.exists(path_new):
-            print(f'deleting {path_ori}...')
-            os.remove(path_ori)
-            
-        else:
-            print(f'moving {path_new}')
-            os.rename(path_ori, path_new)
+        # move file to checked
+        print(f'moving {path_file_player_pay} to {path_file_player_pay_checked}...')
+        mylib.isFolderExist(folder_checked)
+        os.rename(path_file_player_pay, path_file_player_pay_checked)
 
 print(f"[Done] {sys.argv[0]}")
